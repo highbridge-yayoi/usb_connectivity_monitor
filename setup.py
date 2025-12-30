78c3089db6fed406b1feb46d64ed3f0413fc309a
High Bridge <hainu738@gmail.com>
2025-12-31 02:41:55 +0900

    add a command

--- setup.py
+++ setup.py
@@ -4,3 +4,17 @@
 from setuptools import setup
 import os
 from glob import glob
+
+package_name = 'usb_connectivity_monitor'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+    ],
+)
