2f43fdfa20cb04d84624cdcdcee6eae1c8132bf2
High Bridge <hainu738@gmail.com>
2025-12-31 02:42:08 +0900

    add a command

--- setup.py
+++ setup.py
@@ -17,4 +17,16 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ryo takahashi',
+    maintainer_email='hainu738@gmail.com',
+    description='Monitor specific USB device connectivity and publish status.',
+    license='BSD-3-Clause',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'monitor_node = usb_connectivity_monitor.monitor_node:main',
+        ],
+    },
 )
