158da6ce749d44b933945cf8828cd4821eb2361c
High Bridge <hainu738@gmail.com>
2025-12-31 04:57:04 +0900

    add a command

--- launch/monitor.launch.py
+++ launch/monitor.launch.py
@@ -1,2 +1,7 @@
 # SPDX-FileCopyrightText: 2025 highbrige-yayoi <hainu738@gmail.com>
 # SPDX-License-Identifier: BSD-3-Clause
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
